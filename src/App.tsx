ee0944d6619e1a3123b8d9306970948b2c2d87c1
Ahmad Khan <mkhan2@andrew.cmu.edu>
2021-06-01 18:22:19 +0300

    Add inline styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import { Layout } from "antd";
+import * as CSS from "csstype";
 import QueryTable from "./QueryTable";
 const { Header, Content } = Layout;
+
+const h1style: CSS.Properties = {
+  color: "white",
+};
 
 function App() {
   return (
     <Layout>
-      <Header>Mi-INTs DB</Header>
+      <Header>
+        <h1 style={h1style}>Mi-INTs DB</h1>
+      </Header>
       <Content>
         <QueryTable />
       </Content>
