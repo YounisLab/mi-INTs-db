8fb18769bee152a4a5b9836c588ff32e53b32f50
Ahmad Khan <mkhan2@andrew.cmu.edu>
2021-06-04 18:45:18 +0300

    Simulate search behavior with mock data

--- src/QueryTable.tsx
+++ src/QueryTable.tsx
@@ -1,16 +1,47 @@
-import { Input, Table } from "antd";
-import { result } from "./mock";
+import { useState } from "react";
+import { Input, Row, Table } from "antd";
+import * as CSS from "csstype";
+import { result as mockResult } from "./mock";
 
 const { Search } = Input;
 
+const searchBarStyle: CSS.Properties = {
+  width: "40%",
+};
+const rowStyle: CSS.Properties = {
+  marginTop: "16px",
+  width: "100%",
+};
+
 function QueryTable() {
-  const columns = Object.keys(result[0]).map((key) => {
-    return { title: key, dataIndex: key };
-  });
+  const [result, setResult] = useState<Object[]>([]);
+  const [columns, setColumns] = useState<Object[]>([]);
+
+  const handleSearch = (gene: string) => {
+    setResult(
+      mockResult.map((obj, idx) => {
+        return { ...obj, key: idx };
+      })
+    );
+    setColumns(
+      Object.keys(mockResult[0]).map((key) => {
+        return { title: key, dataIndex: key };
+      })
+    );
+  };
+
   return (
     <>
-      <Search />
-      <Table columns={columns} dataSource={result} />
+      <Row style={rowStyle}>
+        <Search onSearch={handleSearch} style={searchBarStyle} />
+      </Row>
+      <Row style={rowStyle}>
+        <Table
+          columns={columns}
+          dataSource={result}
+          locale={{ emptyText: "Search for a gene to view results" }}
+        />
+      </Row>
     </>
   );
 }
