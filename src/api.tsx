920a89135fa058a6bd4228e261d39424f622c5ef
Ahmad Khan <mkhan2@andrew.cmu.edu>
2021-06-08 14:36:13 +0300

    Fix columns param

--- src/api.tsx
+++ src/api.tsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+import qs from "qs";
 
 const apiURL = "http://localhost:8000";
 
 export const getGeneData = (gene: string, columns: object | null = null) => {
   return axios.get(apiURL, {
-    params: { gene, columns },
+    params: { gene, ...(columns && { columns }) },
+    paramsSerializer: function (params) {
+      return qs.stringify(params, { arrayFormat: "repeat" });
+    },
   });
 };
